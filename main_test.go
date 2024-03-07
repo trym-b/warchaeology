300f3bee8ed508c8c6d6e6ec43676f0b096926a7
Trym bremnes <trym.bremnes@gmail.com>
2024-03-07 11:54:04 +0100

    fix: silence output when running `go test`
    This commit fixes the issue where running `go
    test` would write to stdout. This was caused by
    commit 0c94a34011af2c7135fbae6598abc8790e087a25.
    
    The solution is just to redirect `stdout` and
    `stderr` to `nil`.
    
    Solves https://github.com/nlnwa/warchaeology/issues/87

--- main_test.go
+++ main_test.go
@@ -1,12 +1,21 @@
 package main_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/nlnwa/warchaeology/cmd"
 )
 
 func TestEmptyCommandPrompt(t *testing.T) {
+	stdout_backup := os.Stdout
+	stderr_backup := os.Stderr
+	os.Stdout = nil
+	os.Stderr = nil
+
 	shell := cmd.NewCommand()
 	_ = shell.Execute()
+
+	os.Stdout = stdout_backup
+	os.Stderr = stderr_backup
 }
