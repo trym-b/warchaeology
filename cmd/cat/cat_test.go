0beca0dd56e2fd888f3a2249072eb0b85b3f3bb4
Trym bremnes <trym.bremnes@gmail.com>
2024-03-07 11:08:39 +0100

    test 4

--- cmd/cat/cat_test.go
+++ cmd/cat/cat_test.go
@@ -1,16 +1,26 @@
 package cat
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func testDir() string {
 	return "../../test-data"
 }
 
 func BenchmarkValidWarc(b *testing.B) {
+	temp := os.Stdout
+	temp2 := os.Stderr
+	os.Stdout = nil
+	os.Stderr = nil
+
 	config := &config{
 		fileName: testDir() + "/wikipedia/labrador_retriever_0.warc.gz"}
 	_, err := listRecords(config, config.fileName)
 	if err != nil {
 		b.Errorf("Expected no error, got '%s'", err)
 	}
+	os.Stdout = temp
+	os.Stderr = temp2
 }
