dbe5f1d3dd4c7722fe9da197e2cc71d01d5c5ec2
Trym bremnes <trym.bremnes@gmail.com>
2024-03-07 10:50:42 +0100

    test benchmark

--- cmd/cat/cat_test.go
+++ cmd/cat/cat_test.go
@@ -6,11 +6,11 @@ func testDir() string {
 	return "../../test-data"
 }
 
-func TestReadValidWarc(t *testing.T) {
+func BenchmarkValidWarc(b *testing.B) {
 	config := &config{
 		fileName: testDir() + "/wikipedia/labrador_retriever_0.warc.gz"}
 	_, err := listRecords(config, config.fileName)
 	if err != nil {
-		t.Errorf("Expected no error, got '%s'", err)
+		b.Errorf("Expected no error, got '%s'", err)
 	}
 }
