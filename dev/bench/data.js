4e2ee11f21514eadb24c7f663b0f52122ccbb73b
github-action-benchmark <github@users.noreply.github.com>
2024-03-07 12:08:39 +0000

    add Benchmark results (go) benchmark result for e3fa257daaab7baa7fc4e0b3cc90acec84ba8993

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709812351513,
+  "lastUpdate": 1709813319404,
   "repoUrl": "https://github.com/nlnwa/warchaeology",
   "entries": {
     "Benchmark results": [
@@ -54,6 +54,60 @@ window.BENCHMARK_DATA = {
             "value": 4,
             "unit": "auxMetricUnits",
             "extra": "30310 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "trym.bremnes@gmail.com",
+            "name": "Trym bremnes",
+            "username": "trym-b"
+          },
+          "committer": {
+            "email": "trym.bremnes@gmail.com",
+            "name": "Trym bremnes",
+            "username": "trym-b"
+          },
+          "distinct": true,
+          "id": "e3fa257daaab7baa7fc4e0b3cc90acec84ba8993",
+          "message": "moved benchmarks",
+          "timestamp": "2024-03-07T13:07:48+01:00",
+          "tree_id": "81e01e8cbdf5a51b262a089dbe7eea0d519d0d57",
+          "url": "https://github.com/nlnwa/warchaeology/commit/e3fa257daaab7baa7fc4e0b3cc90acec84ba8993"
+        },
+        "date": 1709813318361,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFib10",
+            "value": 312.7,
+            "unit": "ns/op",
+            "extra": "3703177 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20",
+            "value": 39671,
+            "unit": "ns/op",
+            "extra": "30363 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithAuxMetric",
+            "value": 39737,
+            "unit": "ns/op\t         4.000 auxMetricUnits",
+            "extra": "30163 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithAuxMetric - ns/op",
+            "value": 39737,
+            "unit": "ns/op",
+            "extra": "30163 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFib20WithAuxMetric - auxMetricUnits",
+            "value": 4,
+            "unit": "auxMetricUnits",
+            "extra": "30163 times\n4 procs"
           }
         ]
       }
