0369090f782fa232584894d757318dd0f64d312d
Trym bremnes <trym.bremnes@gmail.com>
2024-03-08 07:04:24 +0100

    fixup! fix: silence output when running `go test`
    remove restoration of stdout and stderr

--- main_test.go
+++ main_test.go
@@ -8,14 +8,10 @@ import (
 )
 
 func TestEmptyCommandPrompt(t *testing.T) {
-	stdout_backup := os.Stdout
-	stderr_backup := os.Stderr
 	os.Stdout = nil
 	os.Stderr = nil
 
 	shell := cmd.NewCommand()
 	_ = shell.Execute()
 
-	os.Stdout = stdout_backup
-	os.Stderr = stderr_backup
 }
