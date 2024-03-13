2d8a6b518b61370fc37cc6bc4bfc8ad80abd4842
Trym bremnes <trym.bremnes@gmail.com>
2024-03-13 07:48:24 +0100

    git status

--- cmd/cat/cat_test.go
+++ cmd/cat/cat_test.go
@@ -9,6 +9,6 @@ func BenchmarkDummy(b *testing.B) {
 	// This is a dummy test, it should be replaced with something more
 	// meaningful in a later commit
 	for i := 0; i < b.N; i++ {
-		time.Sleep(1 * time.Second)
+		time.Sleep(1 * time.Millisecond)
 	}
 }
