4e61e40a80a97f9c4ee64be877f86c80d6779d69
Trym Bremnes <trym.bremnes@gmail.com>
2024-03-14 08:43:23 +0100

    create function

--- cmd/cat/cat_test.go
+++ cmd/cat/cat_test.go
@@ -9,6 +9,12 @@ func BenchmarkDummy(b *testing.B) {
 	// This is a dummy test, it should be replaced with something more
 	// meaningful in a later commit
 	for i := 0; i < b.N; i++ {
-		time.Sleep(1 * time.Second)
+		myFunc()
 	}
+}
+
+func myFunc() {
+	// This is a dummy function, it should be replaced with something more
+	// meaningful in a later commit
+	time.Sleep(1 * time.Second)
 }
