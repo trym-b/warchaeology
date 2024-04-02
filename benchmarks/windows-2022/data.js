e823320778281a70713ea46ce8079c8fc74500ba
github-action-benchmark <github@users.noreply.github.com>
2024-04-02 06:13:05 +0000

    add Go Benchmark (go) benchmark result for 6cf2d042ec2bf520baf0b8ec36b70d0640126ea6

---
+++ benchmarks/windows-2022/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1712038384798,
+  "repoUrl": "https://github.com/trym-b/warchaeology",
+  "entries": {
+    "Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "trym.bremnes@gmail.com",
+            "name": "Trym bremnes",
+            "username": "trym-b"
+          },
+          "committer": {
+            "email": "trym.bremnes@gmail.com",
+            "name": "Trym bremnes",
+            "username": "trym-b"
+          },
+          "distinct": true,
+          "id": "6cf2d042ec2bf520baf0b8ec36b70d0640126ea6",
+          "message": "also trigger benchmark with current branch",
+          "timestamp": "2024-04-02T08:10:58+02:00",
+          "tree_id": "8ea4afe66bf0a8500b0c239319edfe466eac3325",
+          "url": "https://github.com/trym-b/warchaeology/commit/6cf2d042ec2bf520baf0b8ec36b70d0640126ea6"
+        },
+        "date": 1712038373793,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDummy",
+            "value": 9815405,
+            "unit": "ns/op",
+            "extra": "148 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
