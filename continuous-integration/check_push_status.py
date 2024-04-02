63f3e7f823a7cb15d2382f0be024cd8a0a5c2f9a
Trym bremnes <trym.bremnes@gmail.com>
2024-04-02 08:53:54 +0200

    debugging 5

--- continuous-integration/check_push_status.py
+++ continuous-integration/check_push_status.py
@@ -1,10 +1,12 @@
-from argparse import Namespace, ArgumentParser
 import sys
+from argparse import ArgumentParser, Namespace
+
 
 def _args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument("--push-status", type=str, required=True)
     return parser.parse_args()
+
 
 def _main():
     push_status = _args().push_status
@@ -15,7 +17,7 @@ def _main():
     else:
         print("Commit has not been pushed successfully")
         sys.exit(1)
-    
+
 
 if __name__ == "__main__":
     _main()
