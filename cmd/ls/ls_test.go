bf3579948088f2d52a88bb1b07942b6c9996a81d
Trym bremnes <trym.bremnes@gmail.com>
2024-04-03 08:18:58 +0200

    write-large-strings?

--- cmd/ls/ls_test.go
+++ cmd/ls/ls_test.go
@@ -19,15 +19,27 @@ func TestConfigReadFileWithError(t *testing.T) {
 	// TODO: check that the result contains the expected values
 }
 
-func BenchmarkReadFileWithError(b *testing.B) {
-	// Stdout is redirected to /dev/null since the benchmarking tool
-	os.Stdout = nil
-	testDataDir := filepath.Join("..", "..", "test-data")
-	warcWithErrors := filepath.Join(testDataDir, "samsung-with-error", "rec-33318048d933-20240317162652059-0.warc.gz")
-	config := &conf{}
-	config.filter = filter.NewFromViper()
-	config.files = []string{warcWithErrors}
+func BenchmarkWriteLongStrings(b *testing.B) {
+	// print extremely long strings instead
+	// of the expected output.
+	b.ReportAllocs()
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = config.readFile(afero.NewOsFs(), warcWithErrors)
+		_ = writeLongStrings(os.Stdout)
 	}
+
+}
+
+func writeLongStrings(out *os.File) error {
+	_, err := out.WriteString("a")
+	if err != nil {
+		return err
+	}
+	for i := 0; i < 1000000; i++ {
+		_, err := out.WriteString("a")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
