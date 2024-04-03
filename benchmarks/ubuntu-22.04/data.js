0885032c112f80db0042788f2545f0753403f890
github-action-benchmark <github@users.noreply.github.com>
2024-04-03 05:31:51 +0000

    add Go Benchmark (go) benchmark result for 30f82bc4a4619681196aafa97845f9ea5d7c008a

--- benchmarks/ubuntu-22.04/data.js
+++ benchmarks/ubuntu-22.04/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712041434688,
+  "lastUpdate": 1712122311576,
   "repoUrl": "https://github.com/trym-b/warchaeology",
   "entries": {
     "Go Benchmark": [
@@ -7014,6 +7014,36 @@ window.BENCHMARK_DATA = {
             "value": 195.9,
             "unit": "ns/op",
             "extra": "6246778 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "trym.bremnes@gmail.com",
+            "name": "Trym Bremnes",
+            "username": "trym-b"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "30f82bc4a4619681196aafa97845f9ea5d7c008a",
+          "message": "Merge pull request #124 from nlnwa/test/add-goleak-to-relevant-tests\n\ntest: avoid leaking goroutines",
+          "timestamp": "2024-04-02T20:08:55+02:00",
+          "tree_id": "41fb0c3f965d35eac666f476f15705cabe4837e1",
+          "url": "https://github.com/trym-b/warchaeology/commit/30f82bc4a4619681196aafa97845f9ea5d7c008a"
+        },
+        "date": 1712122310962,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDummy",
+            "value": 192.7,
+            "unit": "ns/op",
+            "extra": "6315321 times\n4 procs"
           }
         ]
       }
